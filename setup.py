d9caf3320577bd91b747ef27877e98ba14064844
DavidHostler <davidhos117@gmail.com>
2023-09-18 22:31:49 -0400

    Easy-to-use clientside software libraries for use by developers in the AI Space.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 import setuptools
 
 setuptools.setup(
-    name="equoai",
-    version="0.0.2",
-    author="David Hostler",
-    description="Python client for accessing the EquoAI platform and all its core services",
-    packages=["equoai"]
+    name='equoai',
+    version='0.0.2',
+    author='David Hostler',
+    description='Python client for accessing the EquoAI platform and all its core services',
+    packages=['equoai'],
+    download_url = 'https://github.com/DavidHostler/equoai-client',    # I explain this later on
+    install_requires=[            # I get to this in a second
+          'torch',
+          'sentence_transformers',
+          'requests',
+          'tiktoken'
+      ],
 )
